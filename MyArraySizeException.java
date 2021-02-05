b52e052fc3602cc648ebf6b77dbeb88251968dcb
alexrenko <alexrenko@yahoo.com>
2021-02-05 12:40:15 +0500

    practical task created

---
+++ MyArraySizeException.java
@@ -0,0 +1,11 @@
+package FirstSemestr.Java2.Lesson_2;
+
+public class MyArraySizeException extends RuntimeException {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+
+    public MyArraySizeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
